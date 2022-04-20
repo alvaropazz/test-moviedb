bbe94057590d3db9317f60729b5b220f6ffcdbda
a l v a r o • p a z <alvarop3000@gmail.com>
2022-04-20 14:52:22 -0500

    feat: add available scripts to readme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="wrapper" data-testid="wrapper"></div>;
+  return (
+    <div className="wrapper" data-testid="wrapper">
+      hola
+    </div>
+  );
 };
 
 export default App;
